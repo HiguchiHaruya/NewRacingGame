55b4119dbb401a1c30775a9fb48527825130b830
HiguchiHaruya <tongkouyangye@gmail.com>
2025-01-29 19:20:32 +0900

    WayPoint自動生成

---
+++ Assets/Script/EditorExpansion/WayPointEditor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(WayPointManager))]
+public class WayPointEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        WayPointManager manager = (WayPointManager)target;
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("WayPointê∂ê¨",EditorStyles.boldLabel);
+        if (GUILayout.Button("WayPointê∂ê¨"))
+        {
+            manager.GenerateWayPoints();
+            Debug.Log("WayPointê∂ê¨äÆóπ");
+        }
+    }
+}
